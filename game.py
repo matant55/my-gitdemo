47c4f9a0b91021a057882179036b0d83da88c829
matan <matan5514@gmail.com>
2024-01-30 10:21:29 +0200

    we fix the code and create shuffle for return the cards

---
+++ game.py
@@ -0,0 +1,15 @@
+from player import Player
+from cardpack import CardPack
+from typing import List
+
+class CardGame:
+    def __init__(self, cards_for_each_player:int):
+        #create pack
+
+        # create cardpack
+        ...
+    def distibute_cards(self, players:List[Player]):
+        # preform check that there is enough cards for all the players
+        # assign cards from pack to each player using cards_for_each_player number
+        ...
+
---
+++ player.py
@@ -0,0 +1,7 @@
+from typing import List
+from card import Card
+
+class Player:
+    def __init__(self, name: str):
+        self.name = name
+        self.cards:List[Card] = []
